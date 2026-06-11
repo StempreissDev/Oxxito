7fbe926473fd41040bc5211b4286eba633fa53a6
StempreissDev <jcstempreissdev@gmail.com>
2026-06-10 22:26:45 -0600

    Correción de zoom en app

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,10 @@ const geistMono = Geist_Mono({
 
 export const viewport: Viewport = {
   themeColor: "#09090b",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export const metadata: Metadata = {
